5034b6c902a3a5e0916fc684132f36bea07b2962
Henrique Fernandes <hspotted@gmail.com>
2022-07-16 02:50:11 +0100

    Install and configure react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'routes'
 import { ThemeProvider } from 'styled-components'
-
-import { App } from './App'
-
 import { GlobalStyle } from './styles/global'
 import { DefaultTheme } from './styles/themes/default'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={DefaultTheme}>
-      <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>
